5f09a4214475bd1fbcffb29ff2bbacba033dcbc9
Larry <larry.vu@trueplatform.net>
2025-09-15 17:32:45 +0700

    sf socials notification image

--- components/mdx/Image.tsx
+++ components/mdx/Image.tsx
@@ -1,12 +1,17 @@
 import Image, { ImageProps } from "next/image";
+import { cn } from "@/lib/utils";
 
-export default function MDXImage({ src, alt, ...props }: ImageProps) {
+interface MDXImageProps extends ImageProps {
+    className?: string;
+}
+
+export default function MDXImage({ src, alt, className, ...props }: MDXImageProps) {
     return (
         <div className="pt-2 pb-3 flex justify-center items-center">
             <Image
                 src={src}
                 alt={alt}
-                className="rounded-lg w-full h-auto"
+                className={cn("rounded-lg w-full h-auto", className)}
                 {...props}
             />
         </div>
